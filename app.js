3a91e26cf71de83057abf133817cad8266b9e13c
eduvegatalledo <evegat@pucp.edu.pe>
2025-09-09 13:10:10 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,8 @@
 const { createClient } = supabase;
 
 // ⚠️ Reemplaza con tus valores de Supabase:
-const SUPABASE_URL = "https://TU-PROYECTO.supabase.co";
-const SUPABASE_ANON_KEY = "TU_ANON_KEY_PUBLICA";
+const SUPABASE_URL = "https://nzzzeycpfdtvzphbupbf.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im56enpleWNwZmR0dnpwaGJ1cGJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc0NDA3MTIsImV4cCI6MjA3MzAxNjcxMn0.HoAjTwnWdtjueVALlX4-du7uF919QEMj8SS2CHP0N44";
 
 const sb = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
